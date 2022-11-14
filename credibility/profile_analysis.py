7b72a959dd24fc307e0da1507273605e8d3d1ddf
Tachoires Augustin <augustin.tachoires@student-cs.fr>
2022-11-14 16:53:49 +0100

    modif : profile_analysis

--- credibility/profile_analysis.py
+++
@@ -1,8 +0,0 @@
-
-def age_compte(tweet):
-    """
-    Cette fonction renvoie l'age du compte
-    qui a tweeté un tweet
-    :return: int : l'age du compte en jours
-    """
-    return tweet.user.created_at
