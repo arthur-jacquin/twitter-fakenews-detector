18f0dbb2c2e0ec1dda9bf86b45ed4a5d916274f6
Kuhn Eliott <eliott.kuhn@student-cs.fr>
2022-11-16 09:48:48 +0100

    correction: typo

---
+++ tests/test_collect_tweet.py
@@ -0,0 +1,13 @@
+from twitter.collector import collect_tweets
+from pytest import *
+
+
+def test_collect_tweets():
+    tweet = collect_tweets("trump", 3)
+    assert tweet[0] != []
+    assert tweet[1] != []
+    assert tweet[2] != []
+    assert tweet[3] != []
+    assert tweet[4] != []
+    assert tweet[5] != []
+    assert tweet[6] != []
--- tests/test_transform_to_dataframe.py
+++ tests/test_transform_to_dataframe.py
@@ -1,11 +1,11 @@
 from twitter.collector import collect_tweets
-from twitter.collector import transform_to_datframe
+from twitter.collector import transform_to_dataframe
 
 from pytest import *
 
 
-def test_collect():
-    tweet = collect_tweets('trump', 3)
+def test_transform_to_dataframe():
+    tweet = collect_tweets("trump", 3)
     data = transform_to_dataframe(tweet)
     assert 'tweet_textual_content' in data.columns
     assert 'user_id' in data.columns
