1df9896e7eb945c4a7fd397213a9ec883a77bad4
y_chv <yann.chauvard@gmail.com>
2022-11-16 10:58:14 +0100

    Modif test analyse ML

--- tests/test_credibility_analysis.py
+++ tests/test_credibility_analysis.py
@@ -4,9 +4,8 @@ from twitter.collector import collect_tweets,transform_to_dataframe
 
 def test_ml_analysis():
     tweet = collect_tweets('trump',1)
-    """data = transform_to_dataframe(tweet)
-    text = data['tweet_textual_content']"""
-    text = "Je m'appelle John Cagnol"
+    data = transform_to_dataframe(tweet)
+    text = data['tweet_textual_content']
     pred = ml_analysis(text)
     assert 0 <= pred
     assert pred <= 1
