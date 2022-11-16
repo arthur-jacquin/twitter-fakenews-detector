dffbae4b933e71d691a46958ff3be0a666d56ef9
y_chv <yann.chauvard@gmail.com>
2022-11-16 10:38:28 +0100

    Ajout analyse ML et test

--- credibility/credibility_analysis.py
+++ credibility/credibility_analysis.py
@@ -1,6 +1,12 @@
-def ml_analysis(tweet):
+import pickle
+from ML.vectorisation import vectorisation
+
+with open('ML\\fake_news_AI', 'rb') as pipeline:
+    model = pickle.load(pipeline)
+
+def ml_analysis(tweet_txt):
     '''
     Use the ML trained model to estimate the credibility of a tweet.
     Returns a real between 0 and 1, 1 corresponding to most likely fake news.
     '''
-    pass
+    return model.predict_proba(vectorisation(tweet_txt))[0,0]
---
+++ tests/test_credibility_analysis.py
@@ -0,0 +1,12 @@
+from pytest import *
+from credibility.credibility_analysis import ml_analysis
+from twitter.collector import collect_tweets,transform_to_dataframe
+
+def test_ml_analysis():
+    tweet = collect_tweets('trump',1)
+    """data = transform_to_dataframe(tweet)
+    text = data['tweet_textual_content']"""
+    text = "Je m'appelle John Cagnol"
+    pred = ml_analysis(text)
+    assert 0 <= pred
+    assert pred <= 1
