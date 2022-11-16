b180dd47242e534076212d011cd9c69d25866f8c
Kuhn Eliott <eliott.kuhn@student-cs.fr>
2022-11-16 10:47:48 +0100

    modify profile_analysis

--- credibility/profile_analysis.py
+++ credibility/profile_analysis.py
@@ -9,18 +9,18 @@ def number_of_followers(tweet):
     '''
     Get the number of followers of the tweet author.
     '''
-    pass
+    return tweet['user_nb_of_followers']
 
 
 def number_of_following(tweet):
     '''
     Get the number of following of the tweet author.
     '''
-    pass
+    return tweet['user_nb_of_followers']
 
 
-def ratio_of_tweets_retweets(tweets):
+def ratio_of_statuses_account_age(tweets):
     '''
-    Compute the ratio of retweets of the tweet dataframe.
+    Compute the ratio of statuses by the account age.
     '''
     pass
