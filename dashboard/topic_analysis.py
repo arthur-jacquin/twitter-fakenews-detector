b071cc735e6e57f01906fc2dc6c2ec3441c5d179
2022hourida <david.houri@student-cs.fr>
2022-11-16 14:14:51 +0100

    sc

--- dashboard/topic_analysis.py
+++ dashboard/topic_analysis.py
@@ -3,6 +3,9 @@ import dash_daq as daq
 import re
 from credibility.aggregator import credibility
 from twitter.collector import collect_tweets,transform_to_dataframe
+import numpy as np
+
+
 
 
 def topic_analysis(queries, tweet_number):
@@ -21,3 +24,15 @@ def topic_analysis(queries, tweet_number):
             Tweets.append(row)
         for i in range(len(Tweets)):
             Credibility_index.append(credibility(Tweets[i]))
+
+    A=[]
+    for i in range(3):
+        p=np.min(Credibility_index)
+        j=0
+        while Credibility_index[j]!=p:
+            j+=1
+        A.append(Tweets[j],Credibility_index[j])
+        Tweets=Tweets[:j]+ Tweets[j+1:]
+        Credibility_index=Credibility_index[:j]+ Credibility_index[j+1:]
+    
+
