08457101c8368be2b2188e8a81ef58ab08a620d6
2022hourida <david.houri@student-cs.fr>
2022-11-16 13:55:48 +0100

    kjefks

---
+++ django/mysite/polls/templates/static/java.js
@@ -0,0 +1,10 @@
+/*var paragraphe =document.getElementById("paragraphe");
+var mot_clé= prompt("Donnez moi un mot clé");
+var nombre=prompt("Donnez moi un nombre");
+
+paragraphe.innerHTML= 'Vous voulez ' + nombre + 'tweets avec le mot clé ' + mot_clé;
+*/
+
+function myFunction() {
+    alert("Hello from a static file!");
+  }
