2bada7d5d8f1dea2184ddc1d79f50636985e3d04
y_chv <yann.chauvard@gmail.com>
2022-11-16 11:07:11 +0100

    Modif fonction analyse ML

--- credibility/credibility_analysis.py
+++ credibility/credibility_analysis.py
@@ -4,9 +4,9 @@ from ML.vectorisation import vectorisation
 with open('ML\\fake_news_AI', 'rb') as pipeline:
     model = pickle.load(pipeline)
 
-def ml_analysis(tweet_txt):
+def ml_analysis(tweet):
     '''
     Use the ML trained model to estimate the credibility of a tweet.
     Returns a real between 0 and 1, 1 corresponding to most likely fake news.
     '''
-    return model.predict_proba(vectorisation(tweet_txt))[0,0]
+    return model.predict_proba(vectorisation(tweet['tweet_textual_content']))[0,0]
