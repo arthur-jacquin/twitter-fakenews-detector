c69c0042f11b527cdabed949548a602dd705ce5d
Arthur Jacquin <arthur.jacquin@student-cs.fr>
2022-11-16 08:42:33 +0000

    add: skeleton for credibility analysis

---
+++ credibility/aggregator.py
@@ -0,0 +1,10 @@
+from credibility.credibility_analysis import ml_analysis
+from credibility.profile_analysis import account_age, number_of_followers, number_of_following, ratio_of_tweets_retweets
+
+
+def credibility(tweet):
+    '''
+    Compute a real between 0 and 1 reflecting the credibility of a tweet.
+    The higher the number is, the most likely the tweet is fake news.
+    '''
+    pass
---
+++ credibility/profile_analysis.py
@@ -0,0 +1,26 @@
+def account_age(tweet):
+    '''
+    Get the account age of the tweet author.
+    '''
+    pass
+
+
+def number_of_followers(tweet):
+    '''
+    Get the number of followers of the tweet author.
+    '''
+    pass
+
+
+def number_of_following(tweet):
+    '''
+    Get the number of following of the tweet author.
+    '''
+    pass
+
+
+def ratio_of_tweets_retweets(tweets):
+    '''
+    Compute the ratio of retweets of the tweet dataframe.
+    '''
+    pass
