d10bcb3258ffde8dfd19eac819442c1ea51f1dff
y_chv <yann.chauvard@gmail.com>
2022-11-16 14:04:54 +0100

    Modif analyse topic

--- dashboard/topic_analysis.py
+++ dashboard/topic_analysis.py
@@ -9,14 +9,11 @@ def topic_analysis(queries, tweet_number):
     '''
     Compute the analysis of the query, return a DASH element.
     '''
-    #Séparation en sous-requêtes
-    Queries = re.split('/n',queries)
     Credibility_index = []
     Tweets = []
-    for k in range(len(Queries)):
-        data = collect_tweets(Queries[k],tweet_number)
-        data = transform_to_dataframe(data)
-        for _, row in data.iterrows():
-            Tweets.append(row)
+    data = collect_tweets(queries,tweet_number)
+    data = transform_to_dataframe(data)
+    for _, row in data.iterrows():
+        Tweets.append(row)
     for i in range(len(Tweets)):
         Credibility_index.append(credibility(Tweets[i]))
