14ae1470e078a79edf81659a3e497fe08a10152c
Tachoires Augustin <augustin.tachoires@student-cs.fr>
2022-11-16 11:32:43 +0100

    tests

--- tests/test_profile_analysis.py
+++ tests/test_profile_analysis.py
@@ -1,9 +1,6 @@
 from credibility.profile_analysis import account_age, number_of_followers, number_of_following, ratio_of_statuses_account_age
 from twitter.collector import transform_to_dataframe, collect_tweets
 
-# def test_account_age():
-#    pass
-
 for _, row in transform_to_dataframe(collect_tweets('trump', 1)).iterrows():
     tweet = row
 
@@ -12,4 +9,5 @@ def test_number_of_followers():
     assert type(number_of_followers(tweet)) == int
 
 
-test_number_of_followers()
+def test_number_of_following():
+    assert type(number_of_following(tweet)) == int
