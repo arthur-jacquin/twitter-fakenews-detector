d0f5dcadaf70cf7d0d8184333b5b098cb3ffc619
Arthur Jacquin <arthur.jacquin@student-cs.fr>
2022-11-17 07:52:24 +0000

    add: sentiment analysis heatmaps

--- dashboard/topic_analysis.py
+++ dashboard/topic_analysis.py
@@ -1,8 +1,8 @@
 from credibility.aggregator import credibility
+from deep_analysis.sentiment_analysis import sentiment_analysis
 from twitter.collector import collect_tweets, transform_to_dataframe
 
-from dash import Dash, dcc, html, Input, Output, State
-import dash_daq as daq
+from dash import dcc, html
 import pandas as pd
 import plotly.express as px
 
@@ -30,6 +30,8 @@ def topic_analysis(queries, tweet_number):
     # Process tweets
     creds = []
     comments = []
+    polarities = []
+    subjectivities = []
     tweets = []
     rts = []
     for _, tweet in df.iterrows():
@@ -37,6 +39,9 @@ def topic_analysis(queries, tweet_number):
         cred, comment = credibility(tweet)
         creds.append(cred)
         comments.append(comment)
+        polarity, subjectivity = sentiment_analysis(tweet)
+        polarities.append(polarity)
+        subjectivities.append(subjectivity)
         rts.append(tweet['tweet_nb_rt'])
 
     # Sort by credibility
@@ -54,7 +59,7 @@ def topic_analysis(queries, tweet_number):
             'virality': rts,
         }),
         x='credibility', y='virality',
-        range_x=[0., 1.],  range_y=[0., max(rts)],
+        # range_x=[0., 1.], range_y=[0., max(rts)],
         marginal_x="histogram", marginal_y="histogram",
         color_continuous_scale=COLOR,
     )
@@ -68,5 +73,33 @@ def topic_analysis(queries, tweet_number):
     res.append(html.H4('Least trustable tweets'))
     for i in [cred_sorted[j][1] for j in range(-1, -4, -1)]:
         res.append(html_of_tweet(tweets[i], creds[i], comments[i]))
+
+    # Credibility-polarity correlation
+    res.append(html.H3('Credibility-polarity correlation'))
+    cred_polarity_fig = px.density_heatmap(
+        pd.DataFrame({
+            'credibility': creds,
+            'polarity': polarities,
+        }),
+        x='credibility', y='polarity',
+        # range_x=[0., 1.], range_y=[0., max(rts)],
+        marginal_x="histogram", marginal_y="histogram",
+        color_continuous_scale=COLOR,
+    )
+    res.append(dcc.Graph(figure=cred_polarity_fig))
+
+    # Credibility-subjectivity correlation
+    res.append(html.H3('Credibility-subjectivity correlation'))
+    cred_subjectivity_fig = px.density_heatmap(
+        pd.DataFrame({
+            'credibility': creds,
+            'subjectivity': subjectivities,
+        }),
+        x='credibility', y='subjectivity',
+        # range_x=[0., 1.], range_y=[0., max(rts)],
+        marginal_x="histogram", marginal_y="histogram",
+        color_continuous_scale=COLOR,
+    )
+    res.append(dcc.Graph(figure=cred_subjectivity_fig))
 
     return res
