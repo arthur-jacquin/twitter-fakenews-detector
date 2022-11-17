3edb78ee2c367820b731ce92c3c5a331c6e1e28e
Arthur Jacquin <arthur.jacquin@student-cs.fr>
2022-11-17 15:22:00 +0000

    fix: function name in test_tweet_analysis

--- tests/test_tweet_analysis.py
+++ tests/test_tweet_analysis.py
@@ -1,7 +1,7 @@
-from dashboard.tweet_analysis import parse_user_input
+from dashboard.tweet_analysis import parse_tweet_input
 
 
 def test_parsing():
-    assert parse_user_input(
+    assert parse_tweet_input(
         'https://twitter.com/lecartographe/status/1592765408754311168?s=20&t=Ib1_Q7lD7IMtmZVQUAkFQQ') == 1592765408754311168
-    assert parse_user_input('1592765408754311168') == 1592765408754311168
+    assert parse_tweet_input('1592765408754311168') == 1592765408754311168
